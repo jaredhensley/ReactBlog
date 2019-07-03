5b66ced5a1becceefc3c8f1f1c2973b6ba5a672d
Jared Hensley <jahensle85@me.com>
2019-07-02 23:27:42 -0500

    postsReducer added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,6 @@
 import { combineReducers} from 'redux';
-
-const dummy = (action) => {
-    console.log(action);
-    // if (action.type === "FETCH_POSTS") {
-    //     return { posts: ['lol']}
-    // }
-
-    return 'dummy data';
-};
+import postReducer from './postsReducer';
 
 export default combineReducers({
-    dummy
+    posts: postReducer
 });
