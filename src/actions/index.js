d15f633a91ecaa3ca3057ebdd57a9d6d3040623f
Jared Hensley <jahensle85@me.com>
2019-07-04 17:54:34 -0500

    refactor postList reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,6 @@ import jsonplaceholder from '../apis/jsonplaceholder';
 
 export const fetchPosts = () => async dispatch => {
     const response =  await jsonplaceholder.get('/posts');
-    
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
