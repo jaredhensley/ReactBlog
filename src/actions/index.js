c326b4984ca46e4a0eac5a4fcf6e97c5b666f482
Jared Hensley <jahensle85@me.com>
2019-07-05 23:27:29 -0500

    UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@ export const fetchPosts = () => async dispatch => {
 export const fetchUser = (id) => async dispatch => {
     const response =  await jsonplaceholder.get(`/users/${id}`);
 
-    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+    dispatch({ type: 'FETCH_USER', payload: response.data })
 }
 
 
